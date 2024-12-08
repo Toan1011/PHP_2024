fd0b30c2e5e6f5560582b0cd9e5f029eab2d5e6d
Toan1011 <phamtoann1011@gmail.com>
2024-12-09 02:17:38 +0700

    BTVN 9/12

---
+++ BTVN/ktpm2laravel/database/seeders/PostTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Post;
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        for ($i = 0; $i < 100; $i++) {
+            Post::create([
+                'title' => $faker->sentence,
+                'content' => $faker->paragraph
+            ]);
+        }
+    }
+}
